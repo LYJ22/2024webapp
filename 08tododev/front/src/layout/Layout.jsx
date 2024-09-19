8fd9c786d6114e89c4b577cd762bb965ce36147c
LYJ22 <pippi478@gmail.com>
2024-09-19 17:00:37 +0900

    docker and react router2

---
+++ 08tododev/front/src/layout/Layout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="bg-blue-200">
+        <div className="container mx-auto flex justify-between py-4">
+          <h1>logo</h1>
+          <ul className="flex">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container mx-auto py-4">{children}</div>
+      <footer className="bg-gray-400">
+        <div className="container mx-auto py-4">푸터영역</div>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
